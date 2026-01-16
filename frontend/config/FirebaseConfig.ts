5ef9cd9cbf6963744726e9c836a12ab68425aeca
asif7649 <asifahma7761@gmail.com>
2026-01-16 23:14:30 +0530

    change un-use imports

--- frontend/config/FirebaseConfig.ts
+++ frontend/config/FirebaseConfig.ts
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAI, getGenerativeModel, getLiveGenerativeModel, GoogleAIBackend, ResponseModality } from "firebase/ai";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "safar-41ae2.firebaseapp.com",
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import Home from './Pages/home/Home.tsx'
 import VideoSec from './Pages/home/VideoSec.tsx'
 
